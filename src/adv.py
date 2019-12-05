366c2ee52f4b2f0f94bf175cd20fba47fe374092
Sean McDonnell <seanmcdonnell@Seans-MacBook-Pro.local>
2019-12-05 17:27:13 -0500

    Add items to rooms

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,6 @@
 from room import Room
 from player import Player
+from item import Item
 
 # Declare all the rooms
 
@@ -19,7 +20,15 @@ to north. The smell of gold permeates the air."""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""")
+}
+
+# Declare all the items
+
+item = {
+    'gold': Item('Gold', 'An unfathomably large pile of gold'),
+
+    'key': Item('Key', 'A key that grants access to the treasure room')
 }
 
 
@@ -33,6 +42,34 @@ room['overlook'].s_to = room['foyer']
 room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
+
+# Room Map (for developer reference)
+
+########################################
+#    Overlook    #######    Treasure   #
+#                #######               #
+#                #######     (gold)    #
+#                #######               #
+#                #######               #
+########   ###################   #######
+#     Foyer      #######     Narrow    #
+#                #######               #
+#                                      #
+#                #######               #
+#                #######               #
+########   #############################
+#    Outside     #######################
+#                #######################
+#                #######################
+#                #######################
+#                #######################
+########################################
+
+# Add items to rooms
+
+room['treasure'].items.append(item['gold'])
+room['overlook'].items.append(item['key'])
+
 
 #
 # Main
@@ -56,19 +93,19 @@ def initialize():
     ###########################################
     # Set up player and orient them in the game
     ###########################################
-    player_name = input('What is your name?')
+    player_name = input('What is your name?\n')
 
     current_player = Player(player_name, room['outside'])
 
     print(
-        f'Welcome {current_player.get_name()}! You are currently {current_player.get_location().name}'
+        f'Welcome {current_player.get_name()}! Your current location is: {current_player.get_location().name}\n'
     )
 
     ################
     # Start movement
     ################
     def print_current_location():
-        print(f'You are now in the {current_player.get_location().name}')
+        print(f'You are now in the {current_player.get_location().name}\n')
 
     def is_valid_move(move):
         if move != None:
@@ -76,8 +113,14 @@ def initialize():
         else:
             return False
 
+    def room_has_items(room):
+        if len(room.items):
+            return True
+        else:
+            return False
+
     while True:
-        player_input = input('Which direction will you proceed in?')
+        player_input = input('Which direction will you proceed in?\n')
 
         if player_input == 'n' or player_input == 's' or player_input == 'e' or player_input == 'w':
             if player_input == 'n':
@@ -88,9 +131,12 @@ def initialize():
                     )
 
                     print_current_location()
+
+                    if room_has_items(current_player.get_location()):
+                        print('This room contains an item!\n')
                 else:
                     print(
-                        'Path does not exists. Try another direction.'
+                        'Path does not exists. Try another direction.\n'
                     )
 
             elif player_input == 's':
@@ -100,9 +146,12 @@ def initialize():
                     )
 
                     print_current_location()
+
+                    if room_has_items(current_player.get_location()):
+                        print('This room contains an item!\n')
                 else:
                     print(
-                        'Path does not exists. Try another direction.'
+                        'Path does not exists. Try another direction.\n'
                     )
 
             elif player_input == 'e':
@@ -112,9 +161,12 @@ def initialize():
                     )
 
                     print_current_location()
+
+                    if room_has_items(current_player.get_location()):
+                        print('This room contains an item!\n')
                 else:
                     print(
-                        'Path does not exists. Try another direction.'
+                        'Path does not exists. Try another direction.\n'
                     )
             else:
                 if is_valid_move(current_player.get_location().w_to):
@@ -123,9 +175,12 @@ def initialize():
                     )
 
                     print_current_location()
+
+                    if room_has_items(current_player.get_location()):
+                        print('This room contains an item!\n')
                 else:
                     print(
-                        'Path does not exists. Try another direction.'
+                        'Path does not exists. Try another direction.\n'
                     )
         elif player_input == 'q':
             print(
--- src/player.py
+++ src/player.py
@@ -6,6 +6,7 @@ class Player:
     def __init__(self, name, location):
         self.name = name
         self.location = location
+        self.items = []
 
     def get_name(self):
         return self.name
